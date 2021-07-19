197457730f38b1b7c39b86e6b2afde218c6f48b0
Muhammad Zeeshan Ashraf <zeeshanashraf633b@gmail.com>
2021-07-19 23:26:40 +0500

    Pages and Routing

---
+++ pages/events/[slug].js
@@ -0,0 +1,7 @@
+export default function EventsPage() {
+    return (
+        <div>
+            <h1>Event 1</h1>
+        </div>
+    )
+}
---
+++ pages/events/index.js
@@ -0,0 +1,7 @@
+export default function EventsPages() {
+    return (
+        <div>
+            <h1>My Events</h1>
+        </div>
+    )
+}
